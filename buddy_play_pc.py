d98aa67948262e7d20889166d4d43edbb567e042
suu3 <71166763+suu3@users.noreply.github.com>
2021-11-24 14:06:43 +0900

    Update buddy_play_pc.py

--- buddy_play_pc.py
+++ buddy_play_pc.py
@@ -1,39 +1,31 @@
 #This is for PC version
-#Before executing this code, You must install open-cv library first.
+#Before executing this code, You must install opencv-python library first.
 import base64
 import requests
 from time import sleep
 import cv2
+import os
 from recode_audio import recode_audio
 from play_audio import play_audio
 import random
-"""
-필요 import
-"""
 
 run_check = 0
 
-# 첫번째 while 문
-# PIR 부분을 채운다면 그 아래의 두번째 while문 전체를 이 안으로
-"""
-PIR 센서 동작부분
-"""
-
-# 두번째 while 문
-# PIR 센서가 동작하면 작동하는 부분.
-# while 반복문 조건은 PIR에 반응이 있느냐, 없느냐로 수정해야 함
 camera = cv2.VideoCapture(0)
+
 while True:
-    #녹음
+    print("녹음 시작")
+        #녹음
     recode_audio(5)
 
+    print("촬영 시작")
     #촬영
     ret, image = camera.read()
     cv2.imwrite("emotions/frame.png", image)
     img_url = './emotions/frame.png'
 
     if (run_check == 0): # 0은 대기중이란 뜻
-
+        
         # 대화가 한 번 종료됐다가 다시 돌아왔을 경우, 상태정보와 침묵정보를 초기화 해야 하므로 여기서 선언
         state = 0.0
         silence = 0
@@ -42,7 +34,7 @@ while True:
         files = {"images": open(img_url, 'rb'),
                  "wave": open('./audio/user_voice.wav', 'rb'),
                  "state": str(state)}
-
+        print("전송 시작")
         # API 사용. file을 서버로 전송
         res = requests.post('<API 주소>', files=files)
         # json dict() 형식으로 return 되어오는 데이터를 받는다.
@@ -63,15 +55,17 @@ while True:
         run_check = int(data['run_check'])
         state = float(data['state'])
 
+        print("답변 재생 시작")
         # 버디의 답변 재생
         play_audio('./audio/response.wav')
     else: # run_check = 1 = 대화를 시작했다
         # 파이썬 딕셔너리 형식으로 보낼 file 설정
-        files = {"images": open('img_url', 'rb'),
+        files = {"images": open(img_url, 'rb'),
                  "wave": open('./audio/user_voice.wav', 'rb'),
                  "state": str(state),
                  "silence": str(silence)}
 
+        print("전송 시작")
         # API 사용. file을 서버로 전송
         res = requests.post('<API 주소>', files=files)
         data = res.json()
@@ -91,10 +85,12 @@ while True:
         state = float(data['state'])
         silence = int(data['silence'])
         music = int(data['music'])
-
+        
+        print("답변 재생 시작")    
         play_audio('./audio/response.wav')
 
         if music == 1:
+            print("음악")
             # 음악을 재생
             n = random.randrange(1, 7)
             play_audio('./music/music'+n+'.wav')
@@ -106,13 +102,15 @@ while True:
             run_check = 0
             state = 0
 
-    if silence <= 15:
+    if run_check==0 & silence <= 15:
+        print("종료")
         # 침묵으로 빠져나온 것이 아니라, 종료나 음악 키워드로 빠져나온 상태
         # 사용자가 버디 종료를 원한 것이므로 바로 말을 걸면 안되니 일정시간 대기한다
         # 임의로 10분(600초)으로 설정했다.
+
         sleep(600)
 
-    # PIR에서 감지되는 인물이 사라지면 두번째 while문 탈출
-
+if os.path.isfile(img_url):
+    os.remove(img_url)
 camera.release()
 cv2.destroyAllWindows()
